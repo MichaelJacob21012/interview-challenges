79480b876bec10b154644f692adecf52b30bfcea
MichaelJacob21012 <michaeljacob21012@gmail.com>
2021-08-04 10:23:29 +0100

    Initial Attempt

--- javascript-challenge/src/events.js
+++ javascript-challenge/src/events.js
@@ -1,16 +1,16 @@
-/** 
+/**
   An event could look like this:
   ```
   {
     id: 107,
-    startsAt: '2021-01-27T13:01:11Z', 
-    endsAt: '2021-01-27T15:01:11Z', 
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
     title: 'Daily walk',
   }
   ```
 */
 
-/** 
+/**
   Take an array of events and return an object that is a  mapping from the 'day' to the events occuring on that day.
   The keys should be the day-difference to the earliest occuring event.
   Each days events should be sorted in ascending order of startsAt
@@ -34,7 +34,7 @@ const groupEventsByDay = (events) => {
   return events;
 };
 
-/** 
+/**
   Adjust the start and end date of an event so it maintains its total duration, but is moved `toDay`.
   `eventsByDay` should be the same as the return value of `groupEventsByDay`
   `id` will be the event that should be moved
@@ -45,7 +45,7 @@ const groupEventsByDay = (events) => {
   moveEventToDay(
     {
       0: [
-        { id: 106, startsAt: '2021-01-27T13:01:11Z',  endsAt: '2021-01-27T15:01:11Z',  title: 'Daily walk' },      
+        { id: 106, startsAt: '2021-01-27T13:01:11Z',  endsAt: '2021-01-27T15:01:11Z',  title: 'Daily walk' },
       ],
       2: [
         { id: 5676, startsAt: '2021-01-29T13:01:11Z',  endsAt: '2021-01-29T15:01:11Z',  title: 'Daily walk' },
@@ -55,11 +55,11 @@ const groupEventsByDay = (events) => {
     3,
   )
   ```
-  Should return something like 
+  Should return something like
   ```
   {
     0: [
-      { id: 106, startsAt: '2021-01-27T13:01:11Z',  endsAt: '2021-01-27T15:01:11Z',  title: 'Daily walk' },      
+      { id: 106, startsAt: '2021-01-27T13:01:11Z',  endsAt: '2021-01-27T15:01:11Z',  title: 'Daily walk' },
     ],
     3: [
       { id: 5676, startsAt: '2021-01-30T13:01:11Z',  endsAt: '2021-01-30T15:01:11Z',  title: 'Daily walk' },
